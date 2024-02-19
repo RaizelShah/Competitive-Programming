b53f39ecbf39dd2afa3c737162e10ea5291d6bfb
RaizelShah <RaizelShah@users.noreply.github.com>
2024-02-19 15:27:03 +0530

    Solved problem add

--- B_Make_it_Divisible_by_25.cpp
+++ B_Make_it_Divisible_by_25.cpp
@@ -3,8 +3,25 @@
 using namespace std;
 void solve()
 {
-    int n;
-    cin>>n;
+    string s;
+    cin >> s;
+
+    int l = s.size();
+    int ans = l;
+
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = i + 1; j < l; j++)
+        {
+            int num = (s[i] - '0') * 10 + (s[j] - '0');
+            if (num % 25 == 0)
+            {
+                ans = min(ans, (j - i - 1) + (l - 1 - j));
+            }
+        }
+    }
+
+    cout << ans << endl;
 }
 signed main()
 {
