8088e5bb0b254e628efb4c3d434f7bc68d332432
RaizelShah <RaizelShah@users.noreply.github.com>
2024-02-22 23:57:43 +0530

    Modified code

--- B_Good_Arrays.cpp
+++ B_Good_Arrays.cpp
@@ -5,6 +5,11 @@ void solve()
 {
     int n;
     cin >> n;
+    int v[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
     if (n == 1)
     {
         cout << "NO" << endl;
@@ -12,25 +17,23 @@ void solve()
     }
     else
     {
-        vector<int> v(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> v[i];
-        }
         int inc = 0;
         int dec = 0;
+        /*for (auto it : v)
+            cout << it << " ";
+        cout << endl;*/
         for (int i = 0; i < n; i++)
         {
             if (v[i] == 1)
                 inc++;
             else
             {
-                dec += (v[i]-1);
+                dec += (v[i] - 1);
             }
         }
         if (inc <= dec)
             cout << "YES" << endl;
-        else if (inc>dec)
+        else if (inc > dec)
             cout << "NO" << endl;
     }
 }
