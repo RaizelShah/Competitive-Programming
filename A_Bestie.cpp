c20d53ad2e848db7bb029e8aff9f6ac8723386e5
RaizelShah <RaizelShah@users.noreply.github.com>
2024-03-21 20:58:19 +0530

    Incomplete Problem added

---
+++ A_Bestie.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define mod 1000000007
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    fr(0, n)
+    {
+        cin >> arr[i];
+    }
+
+    int min_cost=0;
+    rep(i, 0, n)
+    {
+        rep(j, i + 1, n)
+        {
+            if ()
+        }
+    }
+}
+signed main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
